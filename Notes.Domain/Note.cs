3b062e4b6eaf761f760ba5983ced3ffa10fac554
whyme0 <sparkjetstudiost@gmail.com>
2022-07-24 12:37:12 +0300

    minor model change

--- Notes.Domain/Note.cs
+++ Notes.Domain/Note.cs
@@ -4,8 +4,8 @@
     {
         public Guid UserId { get; set; }
         public Guid Id { get; set; }
-        public string? Title { get; set; }
-        public string? Details { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime? EditDate { get; set; }
 
