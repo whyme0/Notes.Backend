fee174bd4d03143e7f7b95798a0b9e9f90a542dc
whyme0 <sparkjetstudiost@gmail.com>
2022-07-29 19:00:27 +0300

    change method name

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -6,7 +6,7 @@ namespace Notes.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddAssembly(this IServiceCollection services)
+        public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
             return services;
