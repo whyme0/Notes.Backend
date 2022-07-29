f4e1c66e878deb3b85ce72d36a30c53ea5bdeaf2
whyme0 <sparkjetstudiost@gmail.com>
2022-07-29 18:56:13 +0300

    added dto model for web api

---
+++ Notes.WebApi/Models/CreateNoteDTO.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Notes.Application.Commands;
+using Notes.Application.Common.Mappings;
+
+namespace Notes.WebApi.Models
+{
+    public class CreateNoteDTO : IMapWith<CreateNoteCommand>
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateNoteDTO, CreateNoteCommand>();
+        }
+    }
+}
---
+++ Notes.WebApi/Models/UpdateNoteDTO.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Notes.Application.Commands;
+using Notes.Application.Common.Mappings;
+
+namespace Notes.WebApi.Models
+{
+    public class UpdateNoteDTO : IMapWith<UpdateNoteCommand>
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Details { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdateNoteDTO, UpdateNoteCommand>();
+        }
+    }
+}
