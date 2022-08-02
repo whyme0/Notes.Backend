90ff7d5426c1aa88e60c72c23dae5a6e3dc76782
whyme0 <sparkjetstudiost@gmail.com>
2022-08-02 15:49:32 +0300

    create extension for custom exception handler of notes.webapi

---
+++ Notes.WebApi/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Notes.WebApi.Middleware;
+
+namespace Notes.WebApi
+{
+    public static class DependencyInjection
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionHandlerMiddleware>();
+        }
+    }
+}
