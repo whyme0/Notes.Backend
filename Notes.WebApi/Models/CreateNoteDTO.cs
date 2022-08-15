48660241788cc5454329242d00326be86c25d575
whyme0 <sparkjetstudiost@gmail.com>
2022-08-15 18:02:22 +0300

    sets required attribute to title

--- Notes.WebApi/Models/CreateNoteDTO.cs
+++ Notes.WebApi/Models/CreateNoteDTO.cs
@@ -1,11 +1,13 @@
 ﻿using AutoMapper;
 using Notes.Application.Commands;
 using Notes.Application.Common.Mappings;
+using System.ComponentModel.DataAnnotations;
 
 namespace Notes.WebApi.Models
 {
     public class CreateNoteDTO : IMapWith<CreateNoteCommand>
     {
+        [Required]
         public string Title { get; set; }
         public string Details { get; set; }
 
