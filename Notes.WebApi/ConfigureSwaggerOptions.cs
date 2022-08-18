62b1d40006378b9ff7c9afb8f7ad3f6f9e589ed9
whyme0 <sparkjetstudiost@gmail.com>
2022-08-18 16:06:31 +0300

    settings

---
+++ Notes.WebApi/ConfigureSwaggerOptions.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+namespace Notes.WebApi
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private IApiVersionDescriptionProvider _provider;
+        
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                var apiVersion = description.ApiVersion.ToString();
+                options.SwaggerDoc(description.GroupName,
+                    new OpenApiInfo
+                    {
+                        Version = apiVersion,
+                        Title = $"Notes API {apiVersion}",
+                        Description =
+                            "A simple example ASP NET Core Web API. Professional way",
+                        TermsOfService =
+                            new Uri("https://example.com/terms-of-use"),
+                        Contact = new OpenApiContact
+                        {
+                            Name = "FAQ",
+                            Email = string.Empty,
+                            Url =
+                                new Uri("https://example.com/faq")
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "Give feedback",
+                            Url =
+                                new Uri("https://example.com/feedback")
+                        }
+                    });
+
+                options.AddSecurityDefinition($"AuthToken {apiVersion}",
+                    new OpenApiSecurityScheme
+                    {
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.Http,
+                        BearerFormat = "JWT",
+                        Scheme = "bearer",
+                        Name = "Authorization",
+                        Description = "Authorization token"
+                    });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = $"AuthToken {apiVersion}"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+
+                options.CustomOperationIds(apiDescription =>
+                    apiDescription.TryGetMethodInfo(out MethodInfo methodInfo)
+                        ? methodInfo.Name
+                        : null);
+            }
+        }
+    }
+}
