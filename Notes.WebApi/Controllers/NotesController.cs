d98d8b64bada83eb949dbb92f70cd23fdf640a25
whyme0 <sparkjetstudiost@gmail.com>
2022-08-18 15:13:22 +0300

    add versioning for controllers

--- Notes.WebApi/Controllers/NotesController.cs
+++ Notes.WebApi/Controllers/NotesController.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Notes.WebApi.Controllers
 {
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     [Produces("application/json")]
-    [Route("api/[controller]")]
+    [Route("api/{version:apiVersion}/[controller]")]
     [Authorize]
     public class NotesController : ControllerBase
     {
